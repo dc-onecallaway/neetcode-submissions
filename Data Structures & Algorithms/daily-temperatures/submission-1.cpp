4aabd785bcd740796bfea96c61c2cb17ac3afc2e
dc-onecallaway <135661035+dc-onecallaway@users.noreply.github.com>
2026-07-18 17:56:57 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int,int>> temp;
+        vector<int> ans;
+
+        for (int i = temperatures.size() - 1; i >= 0 ; i--) {
+            while (!temp.empty() && temp.top().first <= temperatures[i]) {
+                temp.pop();
+            }
+            if (temp.empty()) {
+                ans.push_back(0);
+            } else {
+                ans.push_back(temp.top().second - i);
+            }
+            temp.push({temperatures[i],i});
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
